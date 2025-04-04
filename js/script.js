0adbfb7c24c25030f0b3a0bf69a10ac8e7906e35
mercadopliz <mercadaodabahia0@gmail.com>
2025-04-03 21:53:59 -0300

    update 1

--- js/script.js
+++ js/script.js
@@ -8,3 +8,42 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll("section");
+
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.style.opacity = 1;
+                }
+            });
+        },
+        { threshold: 0.1 }
+    );
+
+    sections.forEach((section) => {
+        observer.observe(section);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll("nav ul li a");
+
+    links.forEach((link) => {
+        link.addEventListener("click", (event) => {
+            event.preventDefault(); // Evita o comportamento padrão do link
+            const targetId = link.getAttribute("href").substring(1); // Obtém o ID da seção
+            const targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                // Rola suavemente até a seção
+                targetSection.scrollIntoView({
+                    behavior: "smooth",
+                    block: "start",
+                });
+            }
+        });
+    });
+});
